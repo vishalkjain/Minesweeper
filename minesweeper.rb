74ada89025a85f8e5a9081ae5d31bd64a1ab897b
R <student@appacademy.io>
2014-01-06 10:56:09 -0500

    Added Board and Tile classes

--- minesweeper.rb
+++ minesweeper.rb
@@ -6,6 +6,7 @@ class Minesweeper
 
   def run
     until @board.over?
+      @board.display
       puts "Would you like to flag(f) or reveal(r)? "
       action = gets.chomp
       puts "Where?"
@@ -14,6 +15,36 @@ class Minesweeper
     end
     @board.end_condition
   end
+end
 
+class Board
+  BOMB_NUMBER = 10
 
+  def initialize
+    @board_array = Array.new(9){Array.new(9){Tile.new}}
+
+    @masked_board = mask_board
+  end
+
+  def mask_board
+  end
+
+  def display
+    @masked_board.each do |row|
+      puts row
+    end
+  end
+
+end
+
+class Tile
+  attr_reader :value
+
+  def initialize
+    @value = 0
+  end
+
+  def bomb?
+    self.value == "b"
+  end
 end
